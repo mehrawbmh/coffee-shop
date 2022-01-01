d26d6f7efebff14ffa71748c6ac276f1bb1cf8ce
Mehrawbmh <mehrab@mylaptop>
2022-01-02 00:58:54 +0330

    added order.py

---
+++ models/order.py
@@ -0,0 +1,11 @@
+from app import db
+
+
+class Order(db.Model):
+    id = db.Column('id', db.Integer, primary_key=True)
+    number = db.Column('number', db.INTEGER, default=1)
+    timestamp = db.Column('timestamp', db.TIMESTAMP)
+    status = db.Column('status', db.String(40))
+    table = db.Column('table', db.Integer(), db.ForeignKey('table.id'))
+    menu_item = db.Column('item', db.String(30), db.ForeignKey('menu_item.id'))
+
