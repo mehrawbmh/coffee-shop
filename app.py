609b314eb8c06b91a65bdcea83c8be214167d753
majidnabilo <nabilo.majid@gmail.com>
2022-01-02 23:16:00 +0330

    init : header

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
 
 
-app = Flask(__name__, template_folder='templates')
+app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://msyqdpue:EDeAjLM4oerJuTAM1TWit6n6zUiyQEaQ@castor.db.elephantsql.com/msyqdpue'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
