e13ca67fb55c60cc739e9c69ef0dfa68ca2f8d8f
Mehrawbmh <mehrab@mylaptop>
2022-01-02 14:16:36 +0330

    fixed some problems on order.py model

--- models/order.py
+++ models/order.py
@@ -6,6 +6,8 @@ class Order(db.Model):
     number = db.Column('number', db.INTEGER, default=1)
     timestamp = db.Column('timestamp', db.TIMESTAMP)
     status = db.Column('status', db.String(40))
-    table = db.Column('table', db.Integer(), db.ForeignKey('table.id'))
-    menu_item = db.Column('item', db.String(30), db.ForeignKey('menu_item.id'))
+    table = db.Column('table', db.Integer(), db.ForeignKey('table.id'), nullable=False)
+    menu_item = db.Column('item', db.String(30), db.ForeignKey('menu_item.id'), nullable=False)
 
+    def __str__(self):
+        return f"Order: id>{self.id} | item>{self.menu_item} | table number>{self.table}"
