3ee1344977f3e94177d8af96bd977d13082e1d1f
Ali.es <ali.esmailnejad96@gmail.com>
2022-01-03 15:46:32 +0330

    fix: id primary_key param name

--- models/cashier.py
+++ models/cashier.py
@@ -1,7 +1,8 @@
 from app import db
 
+
 class Cashier(db.Model):
-    id = db.Column(db.Integer, primery_key=True)
+    id = db.Column(db.Integer, primary_key=True)
     first_name = db.Column(db.String(40))
     last_name = db.Column(db.String(40))
     phone_number = db.Column(db.String(20), unique=True)
