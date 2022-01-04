76f52dc2c59c0496485c7a68359667b3c96051d3
Ali.es <ali.esmailnejad96@gmail.com>
2022-01-05 01:06:17 +0330

    fix: set password nullable False add: username field nullable False

--- models/cashier.py
+++ models/cashier.py
@@ -3,11 +3,12 @@ from app import db
 
 class Cashier(db.Model):
     id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(40), nullable=False)
     first_name = db.Column(db.String(40))
     last_name = db.Column(db.String(40))
     phone_number = db.Column(db.String(20), unique=True)
     email = db.Column(db.String(50), unique=True)
-    password = db.Column(db.String(30))
+    password = db.Column(db.String(30), nullable=False)
 
     def __repr__(self):
         return f"id is {self.id} :name : {self.first_name} {self.last_name}"
