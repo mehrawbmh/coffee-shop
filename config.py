85abe2630d82b4396420333c46e1c4fb5c299e8a
Mehrawbmh <mehrab@mylaptop>
2022-01-05 00:26:05 +0330

    add: config.py

---
+++ config.py
@@ -0,0 +1,6 @@
+# from flask import Flask
+# from flask_sqlalchemy import SQLAlchemy
+# app = Flask(__name__, template_folder='templates')
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://msyqdpue:EDeAjLM4oerJuTAM1TWit6n6zUiyQEaQ@castor.db.elephantsql.com/msyqdpue'
+# app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+# db = SQLAlchemy(app)
