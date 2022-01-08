1c21dbd37dde99ee4453a895493545994e16bdac
Ali.es <ali.esmailnejad96@gmail.com>
2022-01-09 01:11:46 +0330

    fix: remove username default value

--- register.py
+++ register.py
@@ -9,16 +9,15 @@ hash_func = hashlib.sha256()
 
 @app.cli.command('create-cashier')
 def create_cashier():
-    username = input('>>> Username [default=cashier]: ')
+    username = input('>>> Username : ')
     hash_func.update(bytes(getpass('>>> Password: '), 'utf-8'))
     password = hash_func.hexdigest()
     email = input('>>> Email [optional]: ')
     firstname = ''
     lastname = ''
     phone = ''
-    if password:
+    if password and username:
         try:
-            username = 'cashier' if username == '' else username
             user_cashier = Cashier(username=username, password=password, email=email, phone_number=phone,
                                    first_name=firstname, last_name=lastname)
             db.session.add(user_cashier)
