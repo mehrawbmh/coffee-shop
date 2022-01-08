3a6a20a8cea4bb733d04d62a4206a95b2055f410
Ali.es <ali.esmailnejad96@gmail.com>
2022-01-09 01:00:18 +0330

    fix: change cashier model password len to 100

--- models/cashier.py
+++ models/cashier.py
@@ -8,7 +8,7 @@ class Cashier(db.Model):
     last_name = db.Column(db.String(40), nullable=False)
     phone_number = db.Column(db.String(20), nullable=False, unique=True)
     email = db.Column(db.String(50), nullable=False, unique=True)
-    password = db.Column(db.String(30), nullable=False)
+    password = db.Column(db.String(100), nullable=False)
 
     def __repr__(self):
         return f"id is {self.id} :name : {self.first_name} {self.last_name}"
