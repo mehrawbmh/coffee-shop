8e5d6a46e58080a59456462295f56d13fc8473b3
Amirhosein Bahrami <amirhosein.bahrami.k77@gmail.com>
2022-01-11 02:15:08 +0330

    database defaults added

--- models/orders.py
+++ models/orders.py
@@ -6,13 +6,13 @@ from config import db
 class Order(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     basket_id = db.Column(db.Integer, db.ForeignKey('basket.id'))
-    created_at = db.Column(db.DateTime, nullable=True, default=datetime.now)
-    updated_at = db.Column(db.DateTime, nullable=True, default=datetime.now)
-    total_price = db.Column(db.Integer, nullable=True, default=0)
-    total_price_discount = db.Column(db.Integer, nullable=True, default=0)
-    estimated_waiting_time = db.Column(db.DateTime, nullable=True, default=datetime.now)
-    is_paid = db.Column(db.Boolean, nullable=True, default=False)
-    is_finished = db.Column(db.Boolean, nullable=True, default=False)
+    created_at = db.Column(db.DateTime, nullable=False, default=datetime.now)
+    updated_at = db.Column(db.DateTime, nullable=False, default=datetime.now)
+    total_price = db.Column(db.Integer, nullable=False, default=0)
+    total_price_discount = db.Column(db.Integer, nullable=False, default=0)
+    estimated_waiting_time = db.Column(db.DateTime, nullable=False, default=datetime.now)
+    is_paid = db.Column(db.Boolean, nullable=False, default=False)
+    is_finished = db.Column(db.Boolean, nullable=False, default=False)
 
     def __init__(self, basket_id):
         self.basket_id = basket_id
