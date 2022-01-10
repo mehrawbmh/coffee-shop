e0094bd3d8248cc6acb5a9457a216550ecb1c3cd
Ali.es <ali.esmailnejad96@gmail.com>
2022-01-10 23:55:22 +0330

    add: login url rule

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
-from view_functions import index, get_comment
+from view_functions import index, get_comment, cashier_login
 from config import app
 
 app.add_url_rule('/', 'index', index, methods=['GET', 'POST'])
 app.add_url_rule('/comment', 'comment', get_comment, methods=['POST'])
+
+
+app.add_url_rule('/login', 'login', cashier_login, methods=['GET', 'POST'])
 
 if __name__ == '__main__':
     app.run()
